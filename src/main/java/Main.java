412f7ad6dd47e6852aaaa300436f34b513084b3a
JoannaMorgas <asia.morgas@gmail.com>
2022-04-24 15:16:54 +0200

    add printing method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+       printSomething();
+    }
+
+    public static void printSomething() {
         System.out.println("A project runnable on jenkins");
     }
 }
